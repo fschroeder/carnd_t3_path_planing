7db1c0a6ec8b214eac00be3eec90cdb224d54202
Fabian Schroeder <fabian.schroeder@gmail.com>
2019-01-19 20:50:19 +0100

    Added spline for trajectory calculation.

--- src/main.cpp
+++ src/main.cpp
@@ -240,22 +240,120 @@ int main() {
 
           	json msgJson;
 
-          	vector<double> next_x_vals;
-          	vector<double> next_y_vals;
-
             // FStart
 
-            double dist_inc = 0.447; // Vmac: 50 Mph
+            int prev_size = previous_path_x.size();
 
-            for(int i = 0; i < 50; i++)
+            // Start in lane^
+            int lane = 1;
+
+            // reference velocity to target
+            double ref_vel = 49.5;
+
+            vector<double> ptsx;
+            vector<double> ptsy;
+
+            double ref_x = car_x;
+            double ref_y = car_y;
+            double ref_yaw = deg2rad(car_yaw);
+
+            std::cout << "Prev_Size: " << prev_size << std::endl;
+
+            if(prev_size < 2){
+                double prev_car_x = car_x - cos(car_yaw);
+                double prev_car_y = car_y - sin(car_yaw);
+
+                ptsx.push_back(prev_car_x);
+                ptsx.push_back(car_x);
+
+                ptsy.push_back(prev_car_y);
+                ptsy.push_back(car_y);
+            }
+            else{
+
+                ref_x = previous_path_x[prev_size-1];
+                ref_y = previous_path_y[prev_size-1];
+
+                double ref_x_prev = previous_path_x[prev_size - 2];
+                double ref_y_prev = previous_path_y[prev_size - 2];
+                ref_yaw = atan2(ref_y - ref_y_prev, ref_x - ref_x_prev);
+
+                ptsx.push_back(ref_x_prev);
+                ptsx.push_back(car_x);
+
+                ptsy.push_back(ref_y_prev);
+                ptsy.push_back(car_y);
+            }
+
+            // Infernet add evenly 30m spaced points ahead of the startig reference
+            vector<double> next_wp0 = getXY(car_s + 30, (2 + 4 * lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+            vector<double> next_wp1 = getXY(car_s + 60, (2 + 4 * lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+            vector<double> next_wp2 = getXY(car_s + 90, (2 + 4 * lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+
+            ptsx.push_back(next_wp0[0]);
+            ptsx.push_back(next_wp1[0]);
+            ptsx.push_back(next_wp2[0]);
+
+            ptsy.push_back(next_wp0[1]);
+            ptsy.push_back(next_wp1[1]);
+            ptsy.push_back(next_wp2[1]);
+
+            for(int i = 0; i < ptsx.size(); i++)
             {
-                double next_s = car_s + (i+1)*dist_inc;
-                double next_d = 6;
+                double shift_x = ptsx[i]-ref_x;
+                double shift_y = ptsy[i]-ref_y;
 
-                vector<double> xy = getXY(next_s, next_d, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+                ptsx[i] = (shift_x * cos(0-ref_yaw) - shift_y * sin(0-ref_yaw));
+                ptsy[i] = (shift_x * sin(0-ref_yaw) + shift_y * cos(0-ref_yaw));
 
-                next_x_vals.push_back(xy[0]);
-                next_y_vals.push_back(xy[1]);
+            }
+
+            // Create a spline
+            tk::spline s;
+
+            // Add the x and y points to the spline
+            //std::cout << ptsx.size() << ":" << ptsy.size() << std::endl;
+            s.set_points(ptsx, ptsy);
+
+            // define the actual x and y points we will use for the planner
+            vector<double> next_x_vals;
+            vector<double> next_y_vals;
+
+            // Start with all of the previous points from last time
+            for(int i = 0; i < previous_path_x.size(); i++){
+                next_x_vals.push_back(previous_path_x[i]);
+                next_y_vals.push_back(previous_path_y[i]);
+            }
+
+            // Calculate how to break up spline points so that we travel at our
+            // desired reference velocity
+
+            double target_x = 30;
+            double target_y = s(target_x);
+            double target_distance = distance(0, 0, target_x, target_y);
+
+            double x_step = 0;
+
+            // fill up the rest of the path planner
+            for(int i = 0; i <= 50 - previous_path_x.size(); i++){
+                double N = (target_distance / (.02 *ref_vel / 2.24));
+                double x_point = x_step + target_x/N;
+                double y_point = s(x_point);
+
+                x_step = x_point;
+
+                double x_ref = x_point;
+                double y_ref = y_point;
+
+                // rotate back after rotaining it earlier
+                x_point = (x_ref * cos(ref_yaw) - y_ref * sin(ref_yaw));
+                y_point = (x_ref * sin(ref_yaw) + y_ref * cos(ref_yaw));
+
+                x_point += ref_x;
+                y_point += ref_y;
+
+                next_x_vals.push_back(x_point);
+                next_y_vals.push_back(y_point);
             }
 
             // FEnd
